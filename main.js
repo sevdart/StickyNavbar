60fe3177462dc83e2f0b972443950c09fb333c08
sevdart <sariyeva.sevdadu@gmail.com>
2022-09-25 15:18:14 +0400

    Sticky navbar has been added

---
+++ main.js
@@ -0,0 +1,14 @@
+const navbarEl=document.querySelector(".navbar");
+
+
+const bottomContainerEl=document.querySelector(".bottom-container");
+
+window.addEventListener('scroll',() => {
+    if(window.scrollY > bottomContainerEl.offsetTop-navbarEl.offsetHeight){
+        navbarEl.classList.add("active")
+    }
+    else{
+        navbarEl.classList.remove("active")
+    }
+
+})
